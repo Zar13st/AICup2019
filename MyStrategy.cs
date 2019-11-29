5246d44838131f5632bd5a962087225c89046081
LimaevAV <Limaevav@gmail.com>
2019-11-30 00:40:34 +0300

    add ShootProvider

--- MyStrategy.cs
+++ MyStrategy.cs
@@ -8,16 +8,19 @@ namespace AiCup2019
         private readonly AimProvider _aimProvider = new AimProvider();
         private readonly MovementTargetProvider _movementTargetProvider = new MovementTargetProvider();
         private readonly JumpProvider _jumpProvider = new JumpProvider();
+        private readonly ShootProvider _shootProvider = new ShootProvider();
 
         public UnitAction GetAction(Unit unit, Game game, Debug debug)
         {
             var (targetPos, nearestEnemy) = _movementTargetProvider.GetTarget(unit, game);
 
-            debug.Draw(new CustomData.Log("Target pos: " + targetPos));
+            debug.Draw(new CustomData.Log($"X: {unit.Position.X:F1}, Y: {unit.Position.Y:F1}"));
 
             var aim = _aimProvider.GetAim(unit, nearestEnemy);
 
             var jump = _jumpProvider.GetJump(game, unit, targetPos);
+
+            var shoot = _shootProvider.GetShoot(unit);
 
             var action = new UnitAction
             {
@@ -25,7 +28,7 @@ namespace AiCup2019
                 Jump = jump,
                 JumpDown = !jump,
                 Aim = aim,
-                Shoot = unit.Weapon.HasValue && unit.Weapon?.Typ != WeaponType.RocketLauncher,
+                Shoot = shoot,
                 SwapWeapon = !unit.Weapon.HasValue || unit.Weapon?.Typ == WeaponType.RocketLauncher,
                 PlantMine = false
             };
---
+++ Providers/ShootProvider.cs
@@ -0,0 +1,14 @@
+﻿using AiCup2019.Model;
+
+namespace AiCup2019.Providers
+{
+    public class ShootProvider
+    {
+        public bool GetShoot(Unit player)
+        {
+            if (!player.Weapon.HasValue || player.Weapon?.Typ == WeaponType.RocketLauncher) return false;
+
+            return true;
+        }
+    }
+}
