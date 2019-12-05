2260339621186dc5e25c909a51ed7406799aa102
LimaevAV <Limaevav@gmail.com>
2019-12-05 23:50:51 +0300

    fix shoot

--- Providers/ShootProvider.cs
+++ Providers/ShootProvider.cs
@@ -20,108 +20,156 @@ namespace AiCup2019.Providers
             double enemyXdouble = target.Position.X;
             double enemyYdouble = target.Position.Y + 1;
 
-            int dirX = playerXdouble < enemyXdouble ? 1 : -1;
-            int dirY = playerYdouble < enemyYdouble ? 1 : -1;
-
+            //цель на одной прямой
             var shootAngle = GetAngle(playerXdouble, playerYdouble, enemyXdouble, enemyYdouble);
             if (double.IsInfinity(shootAngle))
             {
-                if (playerX == enemyX) return true;
-
-                for (int i = playerX; enemyX - i != dirX; i += dirX)
+                var topAngle = GetAngle(playerXdouble, playerYdouble, playerX + 1, playerY + 1);
+                if (double.IsInfinity(topAngle))
                 {
-                    if (game.Level.Tiles[i][playerY] == Tile.Wall || game.Level.Tiles[i][playerY - 1] == Tile.Wall)
+                    if (playerX < enemyX)
                     {
-                        return false;
+                        for (int i = playerX + 1; i < enemyX; i++)
+                        {
+                            if (game.Level.Tiles[i][playerY - 1] == Tile.Wall) return false;
+                        }
+                    }
+
+                    if (playerX > enemyX)
+                    {
+                        for (int i = playerX - 1; i > enemyX; i--)
+                        {
+                            if (game.Level.Tiles[i][playerY - 1] == Tile.Wall) return false;
+                        }
                     }
                 }
+
+                if (playerX < enemyX)
+                {
+                    for (int i = playerX + 1; i < enemyX; i++)
+                    {
+                        if (game.Level.Tiles[i][playerY] == Tile.Wall) return false;
+                    }
+                }
+
+                if (playerX > enemyX)
+                {
+                    for (int i = playerX - 1; i > enemyX; i--)
+                    {
+                        if (game.Level.Tiles[i][playerY] == Tile.Wall) return false;
+                    }
+                }
+
                 return true;
             }
 
-            if (playerY == enemyY || playerY + 1 == enemyY)
+            // </
+            if (playerX >= enemyX && playerY > enemyY)
             {
-                for (int i = playerX; enemyX - i != dirX; i += dirX)
+                for (int i = playerX; i >= enemyX; i --)
                 {
-                    if (game.Level.Tiles[i][playerY - 1] == Tile.Wall)
+                    for (int j = playerY; j >= enemyY; j --)
                     {
-                        return false;
-                    }
-                }
+                        if (game.Level.Tiles[i][j] != Tile.Wall) continue;
 
-                return true;
-            }
+                        var top = new Vec2Double(i + 1, j);
+                        var bot = new Vec2Double(i, j + 1);
+                        var isCrossTile = IsCrossTile(playerXdouble, playerYdouble, shootAngle, ref top, ref bot);
 
-            for (int i = playerX; i != enemyX + dirX; i += dirX)
-            {
-                for (int j = playerY; j != enemyY + dirY; j += dirY)
-                {
-                    if (game.Level.Tiles[i][j] == Tile.Wall)
-                    {
-                        bool isCrossTile;
-                        if (dirX == 1)
+                        debug.Draw(new CustomData.Polygon(new ColoredVertex[]
                         {
-                            if (dirY == 1)
-                            {
-                                var top = new Vec2Double(i, j + 1);
-                                var bot = new Vec2Double(i + 1, j);
-                                isCrossTile = IsCrossTile(playerXdouble, playerYdouble, shootAngle, ref top, ref bot);
-
-                                debug.Draw(new CustomData.Polygon(new ColoredVertex[]
-                                {
-                                    new ColoredVertex(new Vec2Float((float)playerXdouble, (float)playerYdouble), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                    new ColoredVertex(new Vec2Float(i , j + 1), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                    new ColoredVertex(new Vec2Float(i +1, j), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                }));
-                            }
-                            else
-                            {
-                                var top = new Vec2Double(i + 1, j + 1);
-                                var bot = new Vec2Double(i, j);
-                                isCrossTile = IsCrossTile(playerXdouble, playerYdouble, shootAngle, ref top, ref bot);
-
-                                debug.Draw(new CustomData.Polygon(new ColoredVertex[]
-                                {
-                                    new ColoredVertex(new Vec2Float((float)playerXdouble, (float)playerYdouble), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                    new ColoredVertex(new Vec2Float(i + 1, j + 1), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                    new ColoredVertex(new Vec2Float(i, j), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                }));
-                            }
-                        }
-                        else
-                        {
-                            if (dirY == 1)
-                            {
-                                var top = new Vec2Double(i, j);
-                                var bot = new Vec2Double(i + 1, j + 1);
-                                isCrossTile = IsCrossTile(playerXdouble, playerYdouble, shootAngle, ref top, ref bot);
-
-                                debug.Draw(new CustomData.Polygon(new ColoredVertex[]
-                                {
-                                    new ColoredVertex(new Vec2Float((float)playerXdouble, (float)playerYdouble), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                    new ColoredVertex(new Vec2Float(i+1, j + 1), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                    new ColoredVertex(new Vec2Float(i, j), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                }));
-                            }
-                            else
-                            {
-                                var top = new Vec2Double(i + 1, j);
-                                var bot = new Vec2Double(i, j + 1);
-                                isCrossTile = IsCrossTile(playerXdouble, playerYdouble, shootAngle, ref top, ref bot);
-
-                                debug.Draw(new CustomData.Polygon(new ColoredVertex[]
-                                {
-                                    new ColoredVertex(new Vec2Float((float)playerXdouble, (float)playerYdouble), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                    new ColoredVertex(new Vec2Float(i, j + 1), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                    new ColoredVertex(new Vec2Float(i+1, j), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
-                                }));
-                            }
-                        }
+                            new ColoredVertex(new Vec2Float((float)playerXdouble, (float)playerYdouble), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                            new ColoredVertex(new Vec2Float(i, j + 1), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                            new ColoredVertex(new Vec2Float(i+1, j), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                        }));
 
                         if (isCrossTile) return false;
                     }
                 }
+
+                return true;
             }
 
+            // <\
+            if (playerX > enemyX && playerY <= enemyY)
+            {
+                for (int i = playerX; i >= enemyX; i--)
+                {
+                    for (int j = playerY; j <= enemyY; j++)
+                    {
+                        if (game.Level.Tiles[i][j] != Tile.Wall) continue;
+
+                        var top = new Vec2Double(i, j);
+                        var bot = new Vec2Double(i + 1, j + 1);
+                        var isCrossTile = IsCrossTile(playerXdouble, playerYdouble, shootAngle, ref top, ref bot);
+
+                        debug.Draw(new CustomData.Polygon(new ColoredVertex[]
+                        {
+                            new ColoredVertex(new Vec2Float((float)playerXdouble, (float)playerYdouble), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                            new ColoredVertex(new Vec2Float(i+1, j + 1), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                            new ColoredVertex(new Vec2Float(i, j), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                        }));
+
+                        if (isCrossTile) return false;
+                    }
+                }
+
+                return true;
+            }
+
+            // \>
+            if (playerX <= enemyX && playerY > enemyY)
+            {
+                for (int i = playerX; i <= enemyX; i++)
+                {
+                    for (int j = playerY; j >= enemyY; j--)
+                    {
+                        if (game.Level.Tiles[i][j] != Tile.Wall) continue;
+
+                        var top = new Vec2Double(i + 1, j + 1);
+                        var bot = new Vec2Double(i, j);
+                        var isCrossTile = IsCrossTile(playerXdouble, playerYdouble, shootAngle, ref top, ref bot);
+
+                        debug.Draw(new CustomData.Polygon(new ColoredVertex[]
+                        {
+                            new ColoredVertex(new Vec2Float((float)playerXdouble, (float)playerYdouble), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                            new ColoredVertex(new Vec2Float(i + 1, j + 1), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                            new ColoredVertex(new Vec2Float(i, j), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                        }));
+
+                        if (isCrossTile) return false;
+                    }
+                }
+
+                return true;
+            }
+
+            // />
+            if (playerX < enemyX && playerY <= enemyY)
+            {
+                for (int i = playerX; i <= enemyX; i++)
+                {
+                    for (int j = playerY; j <= enemyY; j++)
+                    {
+                        if (game.Level.Tiles[i][j] != Tile.Wall) continue;
+
+                        var top = new Vec2Double(i, j + 1);
+                        var bot = new Vec2Double(i + 1, j);
+                        var isCrossTile = IsCrossTile(playerXdouble, playerYdouble, shootAngle, ref top, ref bot);
+
+                        debug.Draw(new CustomData.Polygon(new ColoredVertex[]
+                        {
+                            new ColoredVertex(new Vec2Float((float)playerXdouble, (float)playerYdouble), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                            new ColoredVertex(new Vec2Float(i , j + 1), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                            new ColoredVertex(new Vec2Float(i +1, j), isCrossTile ? new ColorFloat(255,0,0,0.4f) : new ColorFloat(0,255,0,0.4f)),
+                        }));
+
+                        if (isCrossTile) return false;
+                    }
+                }
+
+                return true;
+            }
             return true;
         }
 
@@ -131,9 +179,17 @@ namespace AiCup2019.Providers
 
             var botAngle = GetAngle(playerX, playerY, bot.X, bot.Y);
 
-            var isCrossTile = topAngle >= shootAngle && (botAngle <= shootAngle || botAngle > 10000000 || botAngle < -10000000);
+            if (double.IsInfinity(topAngle))
+            {
+                return botAngle <= shootAngle;
+            }
 
-            return isCrossTile;
+            if (double.IsInfinity(botAngle))
+            {
+                return topAngle >= shootAngle;
+            }
+
+            return (topAngle >= shootAngle || topAngle > 1000000 || topAngle < -1000000) && (botAngle <= shootAngle || botAngle > 1000000 || botAngle < -1000000);
         }
 
         private double GetAngle(double x1, double y1, double x2, double y2)
