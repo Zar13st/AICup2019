edce7f86e96a4efd6e96815272734b46929c8798
LimaevAV <Limaevav@gmail.com>
2019-12-11 20:31:59 +0300

    fix aim line

--- Providers/AimProvider.cs
+++ Providers/AimProvider.cs
@@ -6,7 +6,7 @@ namespace AiCup2019.Providers
     {
         public Vec2Double GetAim(Unit player, Unit nearestEnemy)
         {
-            var aim = new Vec2Double(nearestEnemy.Position.X - player.Position.X, nearestEnemy.Position.Y - player.Position.Y);
+            var aim = new Vec2Double(nearestEnemy.Position.X - player.Position.X, nearestEnemy.Position.Y - player.Position.Y - 0.5);
 
             return aim;
         }
